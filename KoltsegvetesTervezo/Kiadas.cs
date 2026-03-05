fb705de727522eff5ccce467af662328535f5274
KopaszBeci <kopaszbeci@gmail.com>
2026-03-05 18:34:29 +0100

    ui kész

---
+++ KoltsegvetesTervezo/Kiadas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoltsegvetesTervezo
+{
+    internal class Kiadas
+    {
+        public string Kategoria { get; set; }
+        public string Leiras { get; set; }
+        public decimal Osszeg { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
--- KoltsegvetesTervezo/MainWindow.xaml.cs
+++ KoltsegvetesTervezo/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NetworkHelper;
 
 namespace KoltsegvetesTervezo
 {
@@ -20,9 +21,17 @@ namespace KoltsegvetesTervezo
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Kiadas> koltsegek = new List<Kiadas>();
         public MainWindow()
         {
             InitializeComponent();
+            //DataGridFeltoltes();
+        }
+        public void DataGridFeltoltes()
+        {
+            string url = "http://localhost:3000/kiadasok";
+            koltsegek = Backend.GET(url).Send().As<List<Kiadas>>();
+            dg_kiadasok.ItemsSource = koltsegek;
         }
     }
 }
